c0c3b5525c81f225a85eb78a3cec76212702d93f
Fernando Galdino <fernando.galdino@meta.com.br>
2021-06-01 18:05:33 -0300

    Add Longest palindrome substring

---
+++ src/main/java/br/com/jcc/LongestPalindrome.java
@@ -0,0 +1,38 @@
+package br.com.jcc;
+
+public class LongestPalindrome {
+	
+	int resultStart;
+	int resultLength;
+	
+	public static void main(String[] args) {
+		LongestPalindrome longestPalindrome = new LongestPalindrome();
+		String s = "abcdefe";
+		System.out.println(longestPalindrome.longestPalindrome(s));
+	}
+	
+	public String longestPalindrome(String s){
+		int strLength = s.length();
+		if(strLength < 2){
+			return s;
+		}
+		for (int start = 0; start < strLength - 1; start++) {
+			expandRange(s, start, start);
+			expandRange(s, start, start + 1);
+		}
+		
+		return s.substring(resultStart, resultStart + resultLength);
+	}
+
+	private void expandRange(String str, int begin, int end) {
+		while (begin >= 0 && end < str.length() && str.charAt(begin) == str.charAt(end)) {
+			begin--;
+			end++;
+		}
+		if(resultLength < end - begin - 1){
+			resultStart = begin + 1;
+			resultLength = end - begin -1;
+		}
+	}
+
+}
